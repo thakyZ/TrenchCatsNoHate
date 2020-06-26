bb38818a2b38319040ec72e8b8cc989f7d2e9152
thakyZ <thakyz@outlook.com>
2020-06-26 16:10:20 -0700

    Updated diff file

--- dist.bat
+++ dist.bat
@@ -1,8 +1,8 @@
 @echo off
 powershell -NoProfile -Command "Remove-Item -Recurse .\dist"
 powershell -NoProfile -Command "mkdir .\dist"
-powershell -NoProfile -Command "mkdir .\dist\TrenchCatsNoHate"
-powershell -NoProfile -Command "Copy-Item -Recurse .\About .\dist\TrenchCatsNoHate\About"
-powershell -NoProfile -Command "Copy-Item -Recurse .\Patches .\dist\TrenchCatsNoHate\Patches"
-powershell -NoProfile -Command "Copy-Item .\LICENSE .\dist\TrenchCatsNoHate\LICENSE"
+powershell -NoProfile -Command "mkdir \".\dist\Trench Cats And Callistans No Hate\""
+powershell -NoProfile -Command "Copy-Item -Recurse .\About \".\dist\Trench Cats And Callistans No Hate\About\""
+powershell -NoProfile -Command "Copy-Item -Recurse .\Patches \".\dist\Trench Cats And Callistans No Hate\Patches\""
+powershell -NoProfile -Command "Copy-Item .\LICENSE \".\dist\Trench Cats And Callistans No Hate\LICENSE\""
 exit /d 0
